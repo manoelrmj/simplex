7ba7e79116989de9c32825791f0aa21870be8a18
Manoel <manoelrmiranda@gmail.com>
2017-10-23 22:33:31 -0200

    PL primal/dual impelementada

--- simplex.py
+++ simplex.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import sys
 
 class lpInput:
 	lines = 0
 	columns = 0
 	matrix = []
+	operations_matrix = []
 
 def read_file():
-	with open("pl.txt") as f:
+	with open(sys.argv[1]) as f:
 		content = f.readlines()
 	lp = lpInput()
 	lp.lines = int(content[0])+1
 	lp.columns = int(content[1])+1
-	lp.matrix = np.array([[1, 2, 3, 0], [1, 1, 1, 2], [0, -1, 3, 9]])
+	lp.matrix = np.array(eval(content[2]))
 	lp.matrix = lp.matrix.astype(float)
 	return lp
 
@@ -39,18 +41,38 @@ def build_tableaux(lpInput):
 	lpInput.lines = lpInput.matrix.shape[0]
 	lpInput.columns = lpInput.matrix.shape[1]
 
+	#Inicializa matriz de operações
+	lpInput.operations_matrix = np.zeros((lpInput.lines, lpInput.lines-1))
+	id_index_i = 1
+	id_index_j = 0
+	while id_index_j < lpInput.lines-1:
+		lpInput.operations_matrix[id_index_i][id_index_j] = 1
+		id_index_i += 1
+		id_index_j += 1	
+	#print lpInput.operations_matrix
+
 def primal_pivoting(pl):
 	print pl.matrix
-	print "primal_pivoting:"
 	while True: #Executar até não haver mais entrada negativa no c
+		print "\n--"
 		# Escolher coluna:
 		column_index = None
 		for i in range(0, pl.columns-1):
 			if pl.matrix[0][i] < 0:
 				column_index = i
 				break
-			else:
-				return 0
+		if column_index == None: # Não existem mais elementos negativos no c
+			print "Ótimo: {}".format(pl.matrix[0][pl.columns-1])
+			return
+		else: # Verifica os valores de A na coluna correspondente para checar se é ilimitada
+			positive_a = False
+			for i in range(1, pl.lines):
+				if pl.matrix[i][column_index] > 0:
+					positive_a = True
+			if positive_a == False:
+				print "PL ilimitada"
+				return
+		#print "Coluna: {}".format(column_index)
 		# Calcula a razão entre os elementos de 'b' e 'a' e escolhe o menor:
 		min_ratio = None
 		pivot_index = ()
@@ -64,18 +86,92 @@ def primal_pivoting(pl):
 					pivot_index = (i, column_index)
 					pivot_value = pl.matrix[i][column_index]
 		# Transformar o pivot em 1 (e sua linha correspondente)
-		if pivot_index != 1:
+		#if pivot_index != 1:
+		if pivot_value != 1: ##check this##
 			for i in range(0, pl.columns):
 				#print "{}/{}".format(pl.matrix[pivot_index[0]][i], pivot_value)
 				pl.matrix[pivot_index[0]][i] = float(pl.matrix[pivot_index[0]][i]/pivot_value)
+			#Atualiza matriz de operações
+			for i in range(0, pl.operations_matrix.shape[1]):
+				pl.operations_matrix[pivot_index[0]][i] = float(pl.operations_matrix[pivot_index[0]][i]/pivot_value)
 		# Zerar elementos
 		for i in range(0, pl.lines):
 			if i != pivot_index[0]:
 				coefficient = pl.matrix[i][pivot_index[1]]*-1 # Acha o valor que devemos multiplicar a linha do pivot para somar na linha que se deseja zerar
 				for j in range(0, pl.columns):
 					pl.matrix[i][j] = pl.matrix[i][j] + pl.matrix[pivot_index[0]][j]*coefficient
+				# Atualiza matrix de operações
+				for k in range(0, pl.operations_matrix.shape[1]):
+					pl.operations_matrix[i][k] = pl.operations_matrix[i][k] + pl.operations_matrix[pivot_index[0]][k]*coefficient
+		print pl.operations_matrix
+		print pl.matrix
+		print "--"
+
+def dual_pivoting(pl):
+	print "DUAL PIVONTING MODE ENTERED"
+	print pl.operations_matrix
+	print pl.matrix
+	while True: #Executar até não haver mais entrada negativa no c
+		#Escolher linha
+		line_index = None
+		for i in range(1, pl.lines):
+			#print "[{}][{}]".format(i, pl.columns-1)
+			if pl.matrix[i][pl.columns-1] < 0:
+					line_index = i
+					break
+		if line_index == None: # Não existem mais elementos negativos no c
+			print "Ótimo: {}".format(pl.matrix[0][pl.columns-1])
+			return
+		else: # Verifica se existem valores negativos em A
+			negative_a = False
+			for i in range(0, pl.columns):
+				if pl.matrix[line_index][i] < 0:
+					negative_a = True
+					break
+			if negative_a == False:
+				print "PL inviável"
+				return		
+		# Calcula a razão entre os elementos de 'b' e 'a' e escolhe o menor:
+		min_ratio = None
+		pivot_index = ()
+		pivot_value = 0
+		# Calcula a razão entre os elementos de 'b' e '-a' e escolhe o menor:
+		min_ratio = None
+		pivot_index = ()
+		pivot_value = 0
+		for i in range(0, pl.columns-1):
+			if not (pl.matrix[line_index][i] >= 0): # A deve ser negativo
+				ratio = pl.matrix[line_index][pl.columns-1]/(pl.matrix[line_index][i]*-1)
+				print "{}/{}".format(pl.matrix[line_index][pl.columns-1],pl.matrix[line_index][i])
+				if ratio < min_ratio or min_ratio == None:
+					min_ratio = ratio
+					pivot_index = (line_index, i)
+					pivot_value = pl.matrix[line_index][i]
+		print pivot_value
+		print "[{}][{}]".format(pivot_index[0], pivot_index[1])
+		# Transformar o pivot em 1 (e sua linha correspondente)
+		if pivot_value != 1:
+			for i in range(0, pl.columns):
+				#print "{}/{}".format(pl.matrix[pivot_index[0]][i], pivot_value)
+				pl.matrix[pivot_index[0]][i] = float(pl.matrix[pivot_index[0]][i]/pivot_value)
+			#Atualiza matriz de operações
+			for i in range(0, pl.operations_matrix.shape[1]):
+				pl.operations_matrix[pivot_index[0]][i] = float(pl.operations_matrix[pivot_index[0]][i]/pivot_value)
+		# Zerar elementos
+		for i in range(0, pl.lines):
+			if i != pivot_index[0]:
+				coefficient = pl.matrix[i][pivot_index[1]]*-1 # Acha o valor que devemos multiplicar a linha do pivot para somar na linha que se deseja zerar
+				for j in range(0, pl.columns):
+					pl.matrix[i][j] = pl.matrix[i][j] + pl.matrix[pivot_index[0]][j]*coefficient
+				# Atualiza matrix de operações
+				for k in range(0, pl.operations_matrix.shape[1]):
+					pl.operations_matrix[i][k] = pl.operations_matrix[i][k] + pl.operations_matrix[pivot_index[0]][k]*coefficient
+		print pl.operations_matrix
 		print pl.matrix
 
+
+def auxiliar_lp():
+	return 0
 
 def main():
 	lp = read_file()
@@ -83,11 +179,10 @@ def main():
 	build_tableaux(lp)
 	bool_dual = False
 	for i in range(1, lp.lines):
-		if i < 0: # Entrada negativa no vetor B -> PL Dual
+		if lp.matrix[i][lp.columns-1] < 0: # Entrada negativa no vetor B -> PL Dual
 			bool_dual = True
 	if bool_dual:
-		#dual_pivoting(lp)
-		print "DUAL"
+		dual_pivoting(lp)
 	else:
 		primal_pivoting(lp)
 
